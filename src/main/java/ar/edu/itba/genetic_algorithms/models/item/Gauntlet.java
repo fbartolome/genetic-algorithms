8c6ec71f22f6911d95b5a89c45e1608eb89be72d
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-05-24 17:58:18 -0300

    Add items packages

---
+++ src/main/java/ar/edu/itba/genetic_algorithms/models/item/Gauntlet.java
@@ -0,0 +1,19 @@
+package ar.edu.itba.genetic_algorithms.models.item;
+
+/**
+ * Created by jbellini on 24/5/17.
+ */
+public class Gauntlet extends Item {
+    /**
+     * Constructor.
+     *
+     * @param proficiency Item's proficiency.
+     * @param agility     Item's agility.
+     * @param strength    Item's strength.
+     * @param life        Item's life.
+     * @param resistance  Item's resistance.
+     */
+    protected Gauntlet(double proficiency, double agility, double strength, double life, double resistance) {
+        super(proficiency, agility, strength, life, resistance);
+    }
+}
