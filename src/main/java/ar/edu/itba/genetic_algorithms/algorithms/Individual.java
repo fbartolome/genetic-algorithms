6070b71268dcf26960fb265490a46ed3e43892a2
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-05-26 00:57:40 -0300

    Add javadocs to Individual interface

--- src/main/java/ar/edu/itba/genetic_algorithms/algorithms/Individual.java
+++ src/main/java/ar/edu/itba/genetic_algorithms/algorithms/Individual.java
@@ -1,14 +1,20 @@
 package ar.edu.itba.genetic_algorithms.algorithms;
 
 /**
- * Created by jbellini on 24/5/17.
+ * Represents an individual (basic unit of a genetic algorithm).
+ * Each individual is a possible solution.
  */
 public interface Individual {
 
+    /**
+     * @return The fitness of this individual (i.e how good this individual is).
+     */
     double getFitness();
 
+    /**
+     * @return This individual's chromosome.
+     */
     Chromosome getChromosome();
-
 
 
 }
