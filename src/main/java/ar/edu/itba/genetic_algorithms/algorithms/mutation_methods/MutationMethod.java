a2e57e543649fa9e92e31d1b075a8e095b83f020
fbartolome <fbartolome18@gmail.com>
2017-05-28 15:25:34 -0300

    Define MutationMethod interface.

---
+++ src/main/java/ar/edu/itba/genetic_algorithms/algorithms/mutation_methods/MutationMethod.java
@@ -0,0 +1,15 @@
+package ar.edu.itba.genetic_algorithms.algorithms.mutation_methods;
+
+import ar.edu.itba.genetic_algorithms.algorithms.Chromosome;
+
+public interface MutationMethod {
+
+    /**
+     * Mutates a chromosome.
+     *
+     * @param originalChromosome the original chromosome for mutation.
+     * @return mutated chromosome.
+     */
+    Chromosome mutate(Chromosome originalChromosome);   
+
+}
