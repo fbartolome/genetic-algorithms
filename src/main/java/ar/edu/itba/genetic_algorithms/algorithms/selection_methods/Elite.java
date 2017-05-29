9b11ea4c6b3825c7ffe0f93a34a8ef9bc473dc20
fbartolome <fbartolome18@gmail.com>
2017-05-29 14:22:44 -0300

    Fix Elite selection method.

--- src/main/java/ar/edu/itba/genetic_algorithms/algorithms/selection_methods/Elite.java
+++ src/main/java/ar/edu/itba/genetic_algorithms/algorithms/selection_methods/Elite.java
@@ -1,30 +1,26 @@
 package ar.edu.itba.genetic_algorithms.algorithms.selection_methods;
 
 import ar.edu.itba.genetic_algorithms.algorithms.Chromosome;
+import ar.edu.itba.genetic_algorithms.algorithms.Individual;
 import ar.edu.itba.genetic_algorithms.algorithms.Population;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Elite implements SelectionMethod{
 
-
-    //TODO borrar cuando se haya adaptado el otro
     @Override
     public List<Chromosome> select(Population population, int k) {
-        return null;
-    }
+        List<Individual> individuals = population.getIndividuals();
+        individuals.sort(
+                (Individual i1, Individual i2) -> -(new Double(i1.getFitness()).compareTo(new Double(i2.getFitness()))));
 
-//    @Override
-//    public List<ChromosomePair> select(Population population, int k) {
-//        List<Individual> individuals = population.getIndividuals();
-//        individuals.sort(
-//                (Individual i1, Individual i2) -> -(new Double(i1.getFitness()).compareTo(new Double(i2.getFitness()))));
-//
-//        List<ChromosomePair> selectedIndividuals = new LinkedList<>();
-//        //TODO: make variable the number of selected chromosomes
-//        selectedIndividuals.add(new ChromosomePair(individuals.get(0).getChromosome(), individuals.get(1).getChromosome()));
-//        selectedIndividuals.add(new ChromosomePair(individuals.get(2).getChromosome(), individuals.get(3).getChromosome()));
-//        return selectedIndividuals;
-//    }
+        List<Chromosome> selectedChromosomes = new ArrayList<>();
+        for(int i = 0; i < k; i++){
+            selectedChromosomes.add(individuals.get(i).getChromosome());
+        }
+
+        return selectedChromosomes;
+    }
 
 }
