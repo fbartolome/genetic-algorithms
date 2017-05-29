9af5f2632370d5dfddba02ac54815d1247e0214f
natalia navas <natinavas@gmail.com>
2017-05-29 17:54:06 -0300

    bolzmann method implemented

--- src/main/java/ar/edu/itba/genetic_algorithms/algorithms/selection_methods/Boltzmann.java
+++ src/main/java/ar/edu/itba/genetic_algorithms/algorithms/selection_methods/Boltzmann.java
@@ -1,16 +1,21 @@
 package ar.edu.itba.genetic_algorithms.algorithms.selection_methods;
 
 import ar.edu.itba.genetic_algorithms.algorithms.Chromosome;
+import ar.edu.itba.genetic_algorithms.algorithms.Individual;
 import ar.edu.itba.genetic_algorithms.algorithms.Population;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Matcher;
 
 
 ///TODO!!!! https://doc.lagout.org/science/0_Computer%20Science/2_Algorithms/Practical%20Handbook%20of%20GENETIC%20ALGORITHMS%2C%20Volume%20II/ganf5.pdf
-public class Boltzmann implements SelectionMethod {
+public class Boltzmann extends AccumulatedSelectionMethod {
 
     private double t = 0.3;
+
+    private double a = 0.4;
 
     public Boltzmann(double t) {
         this.t = t;
@@ -19,9 +24,35 @@ public class Boltzmann implements SelectionMethod {
     @Override
     public List<Chromosome> select(Population population, int k) {
 
+        List<Double> numerators = new ArrayList<>();
+        double denominator = 0;
+
+        for(Individual individual: population.getIndividuals()){
+            numerators.add(Math.exp(individual.getFitness()/T(population.getGeneration())));
+            denominator += Math.exp(individual.getFitness()/T(population.getGeneration());
+        }
+
+        double accum = 0;
+        int index = 0;
+        HashMap<Individual, Double> accumulatedExpVal = new HashMap<>();
+
+        for(Individual i : population.getIndividuals()){
+            accumulatedExpVal.put(i, accum + numerators.get(index)/denominator);
+            accum += numerators.get(index)/denominator;
+            index++;
+        }
         List<Chromosome> selectedChromosomes = new ArrayList<>();
 
+        for(int i=0; i<k; i++){
+            double rand = Math.random();
+            selectedChromosomes.add(selectChromosomeOnAccumulatedFitnessProbability(rand, accumulatedExpVal));
+        }
 
         return selectedChromosomes;
+    }
+
+    //TODO
+    private double T(int generation){
+        return 1/a*generation;
     }
 }
