358a39450b1087d9d2ad8292c303847f552ac1e3
natalia navas <natinavas@gmail.com>
2017-05-29 14:05:13 -0300

    Class declaration of boltzmann and deterministic tournament selection method

---
+++ src/main/java/ar/edu/itba/genetic_algorithms/algorithms/selection_methods/Boltzmann.java
@@ -0,0 +1,24 @@
+package ar.edu.itba.genetic_algorithms.algorithms.selection_methods;
+
+import ar.edu.itba.genetic_algorithms.algorithms.Chromosome;
+import ar.edu.itba.genetic_algorithms.algorithms.Population;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+///TODO!!!! https://doc.lagout.org/science/0_Computer%20Science/2_Algorithms/Practical%20Handbook%20of%20GENETIC%20ALGORITHMS%2C%20Volume%20II/ganf5.pdf
+public class Boltzmann implements SelectionMethod {
+
+    //TODO parametrizar y probar valores
+    private double t = 0.3;
+
+    @Override
+    public List<Chromosome> select(Population population, int k) {
+
+        List<Chromosome> selectedChromosomes = new ArrayList<>();
+
+
+        return selectedChromosomes;
+    }
+}
---
+++ src/main/java/ar/edu/itba/genetic_algorithms/algorithms/selection_methods/TournamentDeterministic.java
@@ -0,0 +1,13 @@
+package ar.edu.itba.genetic_algorithms.algorithms.selection_methods;
+
+import ar.edu.itba.genetic_algorithms.algorithms.Chromosome;
+import ar.edu.itba.genetic_algorithms.algorithms.Population;
+
+import java.util.List;
+
+public class TournamentDeterministic implements SelectionMethod {
+    @Override
+    public List<Chromosome> select(Population population, int k) {
+        return null;
+    }
+}
