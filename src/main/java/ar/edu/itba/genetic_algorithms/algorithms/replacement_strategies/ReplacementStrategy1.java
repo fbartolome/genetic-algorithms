94926129ce3e22076a40598f59b5decde6b36f6b
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-05-31 03:39:33 -0300

    Add First Replacement method

--- src/main/java/ar/edu/itba/genetic_algorithms/algorithms/replacement_strategies/ReplacementStrategy1.java
+++ src/main/java/ar/edu/itba/genetic_algorithms/algorithms/replacement_strategies/ReplacementStrategy1.java
@@ -5,13 +5,18 @@ import ar.edu.itba.genetic_algorithms.algorithms.engine.Population;
 
 import java.util.List;
 
-
+/**
+ * This class represents the first replacement strategy, which consists of creating a new {@link Population}
+ * replacing the list of {@link Individual} from the actual {@link Population}.
+ */
 public class ReplacementStrategy1 implements ReplacementStrategy {
-
-
     @Override
     public Population replace(Population actualPopulation, List<Individual> offspring) {
-        return null;
+        if (offspring.size() != actualPopulation.getPopulationSize()) {
+            throw new IllegalArgumentException("This replacement strategy requires " +
+                    "a list of offspring of the same size of the population");
+        }
+        return new Population(offspring, actualPopulation);
     }
 
 }
