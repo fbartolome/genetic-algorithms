eec7b8e17d73922725991d8a8c8a905caeb52b0e
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-05-31 04:50:38 -0300

    Change genetic algorithm engine to use java 8 features
    - Use streams and mapping instead of several lists

--- src/main/java/ar/edu/itba/genetic_algorithms/algorithms/engine/GeneticAlgorithmEngine.java
+++ src/main/java/ar/edu/itba/genetic_algorithms/algorithms/engine/GeneticAlgorithmEngine.java
@@ -1,42 +1,88 @@
 package ar.edu.itba.genetic_algorithms.algorithms.engine;
 
 import ar.edu.itba.genetic_algorithms.algorithms.api.AlleleContainerWrapper;
-import ar.edu.itba.genetic_algorithms.algorithms.api.Chromosome;
 import ar.edu.itba.genetic_algorithms.algorithms.api.Individual;
 import ar.edu.itba.genetic_algorithms.algorithms.crossover_strategies.CrossoverStrategy;
 import ar.edu.itba.genetic_algorithms.algorithms.end_conditions.EndingCondition;
 import ar.edu.itba.genetic_algorithms.algorithms.mutation_strategies.MutationStrategy;
 import ar.edu.itba.genetic_algorithms.algorithms.replacement_strategies.ReplacementStrategy;
 import ar.edu.itba.genetic_algorithms.algorithms.selection_strategies.SelectionStrategy;
+import one.util.streamex.StreamEx;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+/**
+ * Genetic Algorithm engine.
+ * This class defines a method for evolving a {@link Population} in order to find a new one with a better fitness.
+ */
 public class GeneticAlgorithmEngine {
 
+    /**
+     * The actual population.
+     */
     private Population population;
 
+    /**
+     * An ending condition strategy.
+     */
     private final EndingCondition endingCondition;
 
+    /**
+     * A selection strategy.
+     */
     private final SelectionStrategy selectionStrategy;
 
+    /**
+     * The amount of {@link Individual} to select.
+     */
     private final int k;
 
+    /**
+     * A crossover strategy.
+     */
     private final CrossoverStrategy crossoverStrategy;
 
+    /**
+     * A mutation strategy.
+     */
     private final MutationStrategy mutationStrategy;
 
+    /**
+     * The probability of mutating.
+     */
     private final double pm;
 
+    /**
+     * A replacement strategy.
+     */
     private final ReplacementStrategy replacementStrategy;
 
+    /**
+     * An object wrapping al containers of alleles.
+     */
     private final AlleleContainerWrapper alleleContainerWrapper;
 
+
+    /**
+     * Constructor.
+     *
+     * @param initialPopulation      The first population.
+     * @param endingCondition        An ending condition strategy.
+     * @param selectionStrategy      A selection strategy.
+     * @param k                      The amount of {@link Individual} to select.
+     * @param crossoverStrategy      A crossover strategy.
+     * @param mutationStrategy       A mutation strategy.
+     * @param pm                     The probability of mutating.
+     * @param replacementStrategy    A replacement strategy.
+     * @param alleleContainerWrapper An object wrapping al containers of alleles.
+     */
     public GeneticAlgorithmEngine(Population initialPopulation, EndingCondition endingCondition,
                                   SelectionStrategy selectionStrategy, int k,
                                   CrossoverStrategy crossoverStrategy, MutationStrategy mutationStrategy,
-                                  double pm, ReplacementStrategy replacementStrategy, AlleleContainerWrapper alleleContainerWrapper) {
+                                  double pm, ReplacementStrategy replacementStrategy,
+                                  AlleleContainerWrapper alleleContainerWrapper) {
         this.population = initialPopulation;
         this.endingCondition = endingCondition;
         this.selectionStrategy = selectionStrategy;
@@ -48,26 +94,23 @@ public class GeneticAlgorithmEngine {
         this.alleleContainerWrapper = alleleContainerWrapper;
     }
 
+    /**
+     * Evolves the first {@link Population} in order to get a new one with a better fitness.
+     *
+     * @return a new {@link Population} with a better fitness.
+     */
     public Population evolve() {
         while (!endingCondition.isSatisfied(population)) {
-            List<Chromosome> selectedChromosomes = selectionStrategy.select(population, k);
-
-            List<Chromosome> offspringChromosomes = new LinkedList<>();
-            for (int i = 0; i < selectedChromosomes.size(); i += 2) {
-                ChromosomePair offspring = crossoverStrategy.crossover(new ChromosomePair(selectedChromosomes.get(i), selectedChromosomes.get(i + 1)));
-                offspringChromosomes.add(offspring.getFirst());
-                offspringChromosomes.add(offspring.getSecond());
-            }
-
-            for (Chromosome chromosome : offspringChromosomes) {
-                if (Math.random() < pm) {
-                    mutationStrategy.mutate(chromosome, alleleContainerWrapper);
-                }
-            }
-
-            List<Individual> offspringIndividuals = offspringChromosomes.stream().parallel().map(population.getCreator()::create).collect(Collectors.toList());
-
-            population = replacementStrategy.replace(population, offspringIndividuals);
+            population = replacementStrategy.replace(population, StreamEx.of(selectionStrategy.select(population, k))
+                    .pairMap((ChromosomePair::new))
+                    .map(crossoverStrategy::crossover)
+                    .flatMap(pair -> Stream.of(pair.getFirst(), pair.getSecond()))
+                    .map(chromosome -> {
+                        if (new Random().nextDouble() < pm) {
+                            mutationStrategy.mutate(chromosome, alleleContainerWrapper);
+                        }
+                        return chromosome;
+                    }).map(each -> population.getCreator().create(each)).collect(Collectors.toList()));
         }
         return population;
     }
