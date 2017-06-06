46f53860d2682bbdcfe0846e2002908355bc6857
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-06-06 04:40:47 -0300

    Add missing javadocs to AccumulatedSelectionMethod.

--- src/main/java/ar/edu/itba/genetic_algorithms/algorithms/selection_strategies/AccumulatedSelectionMethod.java
+++ src/main/java/ar/edu/itba/genetic_algorithms/algorithms/selection_strategies/AccumulatedSelectionMethod.java
@@ -6,15 +6,26 @@ import com.google.common.collect.Multimap;
 
 import java.util.Optional;
 
+/**
+ * Abstract class implementing a method for selecting a {@link Chromosome} based on the accumulated fitness,
+ * in a random way.
+ */
 public abstract class AccumulatedSelectionMethod implements SelectionStrategy {
 
+    /**
+     * Selects a {@link Chromosome} based on the accumulated fitness, in a random way.
+     *
+     * @param rand           The probability.
+     * @param individualList A {@link Multimap} containing {@link Individual}s and their accumulated fitness.
+     * @return The first {@link Chromosome} matching.
+     */
     protected Chromosome selectChromosomeOnAccumulatedFitnessProbability(Double rand,
                                                                          Multimap<Individual, Double> individualList) {
-        Optional<Chromosome> chromosomeOptional = individualList.entries()
+        return individualList.entries()
                 .parallelStream()
                 .filter(each -> each.getValue() > rand)
-                .map(each -> each.getKey().getChromosome()).findFirst();
-
-        return chromosomeOptional.isPresent() ? chromosomeOptional.get() : null;
+                .map(each -> each.getKey().getChromosome())
+                .findFirst()
+                .orElse(null);
     }
 }
